e6da17c0af4f5ed4eb93b2bebc15962ca1435add
qianiaoo <qianiaoo@gmail.com>
2022-06-18 20:33:41 +0900

    test args.js

--- pages/api/[...args].js
+++ pages/api/[...args].js
@@ -1,6 +1,7 @@
 
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
+console.log("asdasd", process.env.FIREBASE_API_DOMAIN);
 export default createProxyMiddleware({
     target: process.env.FIREBASE_API_DOMAIN,
     changeOrigin: true,
